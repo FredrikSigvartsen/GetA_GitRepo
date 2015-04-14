c8121e6543ed4ef8fd3335f683e3dd19869ca932
fredrik1512 <fredrik.sigvartsen@gmail.com>
2015-04-14 08:56:57 +0200

    Konstruktør

--- ForsikringsProgram/src/forsikringsprogram/BilSkademelding.java
+++ ForsikringsProgram/src/forsikringsprogram/BilSkademelding.java
@@ -5,10 +5,33 @@
  */
 package forsikringsprogram;
 
+import javax.swing.*;
+
 /**
- *
+ * Siden skademeldingen er spesiell for biler, er det naturlig å ha en egen klasse som arver Skademelding. 
+ * Et "skjema" vil alltid være på papirform, og fylles ut av to parter under et bilkrasj. 
  * @author fredr_000
  */
-public class BilSkademelding {
+public class BilSkademelding extends Skademelding {
+
+    private ImageIcon krasjSkjema;
+    
+    public BilSkademelding(String skadeType, String beskrivelse, ImageIcon bilde, String vitneKontakt, int takst, int erstatningsbeløp, 
+                            String inntruffetTidspunkt, ImageIcon krasjSkjema) {
+        super(skadeType, beskrivelse, bilde, vitneKontakt, takst, erstatningsbeløp, inntruffetTidspunkt);
+        krasjSkjema = krasjSkjema;
+    }
+
+    public ImageIcon getKrasjSkjema() {
+        return krasjSkjema;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nSkademeldingsskjema for bilkræsj:  " + krasjSkjema;
+    }
+    
+    
+    
     
 }
