8f4e540068d90caae4c5c93ec26d0f5490822f05
Elias Andreassen Thøgersen <elias3004@msn.com>
2015-04-22 15:39:44 +0200

    Fjerna en kommentar

--- ForsikringsProgram/src/forsikringsprogram/SerialiserbarComparator.java
+++ ForsikringsProgram/src/forsikringsprogram/SerialiserbarComparator.java
@@ -8,6 +8,6 @@ public class SerialiserbarComparator<T> implements Comparator<T>, Serializable {
     
     @Override
     public int compare(T t, T t1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
