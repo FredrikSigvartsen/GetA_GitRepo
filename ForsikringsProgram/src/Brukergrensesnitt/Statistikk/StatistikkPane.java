1e52d2077371aabeb636e9be4f8b7b77cf03d902
Elias Andreassen Thøgersen <elias3004@msn.com>
2015-05-11 17:04:59 +0200

    Utseende

--- ForsikringsProgram/src/Brukergrensesnitt/Statistikk/StatistikkPane.java
+++ ForsikringsProgram/src/Brukergrensesnitt/Statistikk/StatistikkPane.java
@@ -9,7 +9,10 @@ import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import static javafx.scene.layout.BorderStroke.THIN;
+import static javafx.scene.layout.BorderStrokeStyle.SOLID;
 import javafx.scene.paint.Color;
+import static javafx.scene.paint.Color.DARKGRAY;
 
 public class StatistikkPane extends GridPane {
     
@@ -23,6 +26,7 @@ public class StatistikkPane extends GridPane {
         soylediagram = new SoylediagramLayout(kundeRegister);
         graf = new GrafLayout(kundeRegister);
         opprettLayout();
+        //setBorder(new Border(new BorderStroke(DARKGRAY,SOLID, new CornerRadii(5), THIN, new Insets(15))));
         //setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
     }//end of constructor
     
@@ -30,10 +34,10 @@ public class StatistikkPane extends GridPane {
      * Oppretter og setter de ulike layoutene under Kundebehandling
      */
     public void opprettLayout(){
-        GridPane.setMargin(soylediagram, new Insets(0,20,0,0));
+        GridPane.setMargin(soylediagram, new Insets(0,34,0,0));
         add(soylediagram,1,1);
         add(new Separator(Orientation.VERTICAL), 2, 1);
-        GridPane.setMargin(graf, new Insets(0,0,0,20));
+        GridPane.setMargin(graf, new Insets(0,0,0,30));
         add(graf,3,1);
         setVgap(70);
     }//end of method opprettLayout()   
